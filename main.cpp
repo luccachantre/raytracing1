5acab64b7e9b0ae3f96039b53b95c95f487cf9c9
Lucca Chantre <luccachantre@gmail.com>
2026-06-20 18:30:49 -0400

    added log lines in the program to show progress of image render

--- main.cpp
+++ main.cpp
@@ -12,10 +12,9 @@ int main()
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++)
-    {
-        for (int i = 0; i < image_width; i++)
-        {
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.0;
@@ -27,4 +26,6 @@ int main()
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
